9bd678cc41a31ef5c771cc239f9a82c7954175d9
BT <bahartofigh@gmail.com>
2025-04-18 14:58:03 +0330

    navbar scroll effect

--- main-projects/ebook-website-starter/js/script.js
+++ main-projects/ebook-website-starter/js/script.js
@@ -1,1 +1,17 @@
 // Add custom JavaScript here
+
+function userScroll() {
+    const navbar = document.querySelector(".navbar");
+
+    window.addEventListener("scroll", () => {
+        if (scrollY >= 50) {
+            navbar.classList.add("bg-dark");
+            navbar.classList.add("sticky-navbar");
+        } else {
+            navbar.classList.remove("bg-dark");
+            navbar.classList.remove("sticky-navbar");
+        }
+    })
+}
+
+document.addEventListener("DOMContentLoaded", userScroll)
